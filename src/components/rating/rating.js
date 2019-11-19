25b7b37d1399a22812aefccd17633a77578927d2
Alex_L <asleonov@inbox.ru>
2019-11-19 19:28:14 +0300

    add Rating component

---
+++ src/components/rating/rating.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import {Button, Typography} from 'antd'
+// import counterDecorator from '../../decorators/counter'
+
+function Rating(props) {
+  const {currentRestaurant} = props
+
+  return (
+    <div>
+      <h2>Рейтинг ресторана</h2>
+
+    </div>
+  )
+}
+
+export default Rating
