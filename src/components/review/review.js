256da0e02010706091b665a23c2a0226a076216d
Yael Dina Simchi <yaeldina.simchi@gmail.com>
2019-11-19 19:27:56 +0300

    fixed styles in reviews

--- src/components/review/review.js
+++ src/components/review/review.js
@@ -1,14 +1,17 @@
 import React from 'react'
-import {Card, Comment, Rate} from 'antd'
+import {Card, Comment, Rate, Typography} from 'antd'
 
 function Review(props) {
   const {review} = props
   return (
     <Card
-      style={{marginBottom: '1em'}}
+      className="card"
       title={<Rate disabled defaultValue={review.rating} />}
     >
-      <Comment author={review.user} content={<p>review.text</p>} />
+      <Comment
+        author={review.user}
+        content={<Typography.Text>{review.text}</Typography.Text>}
+      />
     </Card>
   )
 }
--- src/components/reviews/reviews.js
+++ src/components/reviews/reviews.js
@@ -1,14 +1,18 @@
 import React from 'react'
 import Review from '../review'
+import {Typography} from 'antd'
 
 function Reviews(props) {
   const {reviews} = props
   return (
-    <div>
+    <section className="descriptionSection">
+      <Typography.Title level={3} className="descriptionSection__title">
+        Reviews
+      </Typography.Title>
       {reviews.map(review => (
         <Review key={review.id} review={review} />
       ))}
-    </div>
+    </section>
   )
 }
 
