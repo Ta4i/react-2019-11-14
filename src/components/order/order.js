16838202a081d05ead13192cdb95249cfa7b95e4
andrey <anbirukov@yandex.ru>
2019-11-27 01:22:32 +0300

    Добавлен компонент Order

---
+++ src/components/order/order.js
@@ -0,0 +1,67 @@
+import React, {Component} from 'react'
+import connect from 'react-redux/es/connect/connect'
+import {Card, Typography, Button, Row, Col} from 'antd'
+
+class Order extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      allDishes: Order.findAllDishes(props.restaurants),
+    }
+  }
+
+  static findAllDishes(restaurants) {
+    let dishesArray = restaurants.reduce((acc, restaurant) => {
+      acc.push(...restaurant.menu)
+      return acc
+    }, [])
+    let dishesMap = dishesArray.reduce((map, dish) => {
+      map[dish.id] = dish
+      return map
+    }, [])
+    {
+      console.log(Object.keys(dishesMap).length + ' dishes found')
+    }
+    return dishesMap
+  }
+
+  computeTotalAmount() {
+    return Object.keys(this.props.cart).reduce((acc, dishId) => {
+      return acc + this.state.allDishes[dishId].price * this.props.cart[dishId]
+    }, 0)
+  }
+
+  render() {
+    return (
+      <Card>
+        {Object.keys(this.props.cart).map(dishId => (
+          <div key={dishId}>
+            <Col xs={16} md={16} lg={20} align="left">
+              <span>{this.state.allDishes[dishId].name}</span>
+            </Col>
+            <Col xs={8} md={6} lg={4} align="right">
+              <span>
+                {this.state.allDishes[dishId].price * this.props.cart[dishId]} $
+              </span>
+            </Col>
+          </div>
+        ))}
+        <div key="total" style={{paddingTop: 50}}>
+          <Col xs={16} md={16} lg={20} align="right">
+            <span>ИТОГО</span>
+          </Col>
+          <Col xs={8} md={6} lg={4} align="right">
+            <span>{this.computeTotalAmount()} $</span>
+          </Col>
+        </div>
+      </Card>
+    )
+  }
+}
+
+const mapsStateToProps = (state, ownProps) => ({
+  cart: state.cart || [],
+  restaurants: state.restaurants,
+})
+
+export default connect(mapsStateToProps)(Order)
