3da9364745008fdfe15bf9d8908b208173c2eda0
Yael Dina Simchi <yaeldina.simchi@gmail.com>
2019-11-26 22:11:28 +0300

    add order component & remove from cart

--- src/store/action-creators/index.js
+++ src/store/action-creators/index.js
@@ -4,9 +4,24 @@ export const increment = () => {
   }
 }
 
+export const decrement = () => {
+  return {
+    type: 'DECREMENT',
+  }
+}
+
 export const addToCart = dishId => {
   return {
     type: 'ADD_TO_CART',
+    payload: {
+      id: dishId,
+    },
+  }
+}
+
+export const removeFromCart = dishId => {
+  return {
+    type: 'REMOVE_FROM_CART',
     payload: {
       id: dishId,
     },
--- src/store/reducers/cart.js
+++ src/store/reducers/cart.js
@@ -5,12 +5,25 @@ export const cartReducer = (
   action
 ) => {
   switch (action.type) {
-    case 'ADD_TO_CART':
+    case 'ADD_TO_CART': {
       const {id} = action.payload
       return {
         ...cartState,
         [id]: cartState[id] ? cartState[id] + 1 : 1,
       }
+    }
+    case 'REMOVE_FROM_CART': {
+      const {id} = action.payload
+      if (!cartState[id]) return cartState
+
+      if (cartState[id] === 1) {
+        const {[id]: removedElement, ...rest} = cartState
+        return rest
+      } else {
+        return {...cartState, [id]: cartState[id] - 1}
+      }
+    }
+    default:
   }
   return cartState
 }
