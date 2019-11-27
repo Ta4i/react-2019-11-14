3027dc7d57571db66d4147b93d575de4aa201917
andrey <anbirukov@yandex.ru>
2019-11-27 20:54:07 +0300

    Доработан cartReducer

--- src/store/reducers/cart.js
+++ src/store/reducers/cart.js
@@ -14,10 +14,21 @@ export const cartReducer = (
     }
     case 'REMOVE_FROM_CART': {
       const {id} = action.payload
-      return {
-        ...cartState,
-        [id]: cartState[id] && cartState[id] > 0 ? cartState[id] - 1 : 0,
+
+      console.log('Call remove dish[' + id + '] from cart')
+      const newCartState = {...cartState}
+
+      if (newCartState[id]) {
+        console.log('Previous value dish[' + id + ']=' + newCartState[id])
+        if (newCartState[id] > 0) {
+          newCartState[id]--
+        }
+        if (newCartState[id] === 0) {
+          console.log('Delete dish[' + id + '] from cart')
+          delete newCartState[id]
+        }
       }
+      return newCartState
     }
   }
   return cartState
