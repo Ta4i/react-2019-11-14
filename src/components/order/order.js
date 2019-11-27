d2aa4f0d8f37d0623e4971f300d3bab31527988b
Kate Sava <ekaterinasava@gmail.com>
2019-11-27 18:14:21 +0100

    UPD 'Order' component

--- src/components/order/order.js
+++ src/components/order/order.js
@@ -5,21 +5,26 @@ import styles from './order.module.css'
 import {connect} from 'react-redux'
 
 function Order(props) {
+  const {selectedDishes, totalAmount} = props
+
   return (
     <div className={styles.order}>
       <Typography.Title level={2}>Order</Typography.Title>
       <Card>
-        <div>Total dishes amount: {props.totalAmount}</div>
+        <div>Selected dishes: {selectedDishes}</div>
+        <div>Total dishes amount: {totalAmount}</div>
       </Card>
     </div>
   )
 }
 
 Order.defaultProps = {
+  selectedDishes: 'No selected dishes',
   totalAmount: 0,
 }
 
 Order.propTypes = {
+  selectedDishes: PropTypes.string.isRequired,
   totalAmount: PropTypes.number.isRequired,
 }
 
