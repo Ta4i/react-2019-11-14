b70be3dbfe65d509def22cee0beff748d38d6524
Alex_L <asleonov@inbox.ru>
2019-11-28 16:35:03 +0300

    Fix bug with orderState.visible

--- src/store/reducers/order.js
+++ src/store/reducers/order.js
@@ -1,7 +1,5 @@
 export const orderReducer = (orderState = {visible: false}, action) => {
-  if (action.type === 'OPEN_ORDER') {
-    orderState.visible = true
-  }
+  if (action.type === 'OPEN_ORDER') return {visible: true} //orderState.visible = true - не сработает: в стор запишется, но не перерендерится...
 
   return orderState
 }
