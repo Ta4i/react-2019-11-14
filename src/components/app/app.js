e8409f275ac050966da1e34d1a6f851f8ef6e6a6
sevahx <sevahx@gmail.com>
2019-12-10 23:12:10 +0200

    Add Order & OrderComplete pages

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -7,6 +7,8 @@ import {Provider} from 'react-redux'
 import {BrowserRouter, Route, Switch} from 'react-router-dom'
 import RestaurantPage from '../../routes/restaurant-page'
 import CounterPage from '../../routes/counter-page'
+import OrderComplete from '../../routes/order-complete'
+import Cart from '../cart/cart'
 
 class App extends Component {
   render() {
@@ -20,6 +22,8 @@ class App extends Component {
                 <Switch>
                   <Route path="/counter" exact strict component={CounterPage} />
                   <Route path="/restaurant" component={RestaurantPage} />
+                  <Route path="/order" component={Cart} />
+                  <Route path="/orderComplete" component={OrderComplete} />
                   <Route path="/" render={() => <h1>Page not found</h1>} />
                 </Switch>
               </Layout.Content>
